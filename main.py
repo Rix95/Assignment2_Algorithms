420ee7b637f5c3decbb99c14707e3515402b27f8
Rix95 <ricalrax@gmail.com>
2023-02-16 18:15:59 -0600

    Added stuff stuffier

--- main.py
+++ main.py
@@ -3,43 +3,24 @@ import math
 from tabulate import tabulate
 import numpy as np
 
-maxint = math.inf
+print(
+    "================================================================================================================")
+print("                                 Developer Ricardo Elizondo")
+print(
+    "================================================================================================================")
+print("                              School name: Texas A&M San Antonio")
+print("                                          Algorithms")
+print("                                          Assignment 2")
+print("                                 Semester/Year: Spring 2023")
+print(
+    "================================================================================================================")
+print("")
 
 
-def max_subarray_sum(array):
-    transformed_array = []
-    for i in range(len(array) - 1):
-        transformed_array.append(array[i + 1] - array[i])
-
-    max_so_far = -maxint - 1
-    max_ending_here = 0
-
-    for i in range(0, len(transformed_array)):
-        max_ending_here = max_ending_here + transformed_array[i]
-        if max_so_far < max_ending_here:
-            max_so_far = max_ending_here
-
-        if max_ending_here < 0:
-            max_ending_here = 0
-    return max_so_far
-
-
-# generate new random list with assigned values
-def random_list_generator(list_size, min_value, max_value):
-    random_generated_list = []
-    for i in range(0, list_size):
-        n = random.randint(min_value, max_value)
-        random_generated_list.append(n)
-
-    #  print(random_generated_list)
-    return random_generated_list
-
-
-# find maximum subarray implemented from pseudocode,
+# find maximum subarray implemented from pseudocode given.
 def find_maximum_subarray(number_array: list, low, high):
     # Bring error if array is empty
     if len(number_array) <= 0:
-        print("Not enough elements to compare")
         return 0, 0, 0
     else:
         if high == low:
@@ -59,7 +40,7 @@ def find_maximum_subarray(number_array: list, low, high):
                 return cross_low, cross_high, cross_sum
 
 
-# find max crossing subarray implemented from pseudocode.
+# find max crossing subarray implemented from pseudocode given.
 def find_max_crossing_subarray(number_array, low: int, mid: int, high: int):
     left_sum = right_sum = -math.inf
     current_left_sum = current_right_sum = max_left_index = max_right_index = 0
@@ -79,6 +60,7 @@ def find_max_crossing_subarray(number_array, low: int, mid: int, high: int):
     return max_left_index, max_right_index, left_sum + right_sum
 
 
+# class to hold values of Max profit and also print table. recursion side is out of scope.
 class MaxProfit:
 
     # Initialize list.
@@ -88,41 +70,60 @@ class MaxProfit:
 
         self.price_array = price_array
 
-        self.daily_change_list = []
+        self.daily_change_list = []  # array to store daily changes
         self.get_daily_changes()
         # low and high indexes for the array
         self.low_index = 0
         self.high_index = len(self.daily_change_list) - 1
         self.mid_index = (self.low_index + self.high_index) // 2
+
+        # local max indexes including best choice
         self.left_max = ()
         self.right_max = ()
         self.cross_max = ()
         self.best_choice = (0, 0, 0)
+
         self.find_maximum_values()
 
+    #  get all price changes from price array.
     def get_daily_changes(self):
         for idx in range(len(self.price_array) - 1):
             self.daily_change_list.append(self.price_array[idx + 1] - self.price_array[idx])
         return
 
+    #  find maximum values for left, right and cross, calls to recursive outer function.
     def find_maximum_values(self):
         # Bring error if array is empty
         if len(self.price_array) <= 0:
             return
         else:
-
             self.left_max = find_maximum_subarray(self.daily_change_list, self.low_index, self.mid_index)
-            print("chill")
             self.right_max = find_maximum_subarray(self.daily_change_list, self.mid_index + 1, self.high_index)
-            print("bro")
             self.cross_max = find_max_crossing_subarray(self.daily_change_list, self.low_index, self.mid_index,
                                                         self.high_index)
-            print(type(self.left_max), type(self.right_max), type(self.cross_max))
 
+            #  sorts tuples to select best choice available, store in variable
             tuple_list = sorted([self.left_max, self.right_max, self.cross_max], key=lambda x: x[2], reverse=True)
             self.best_choice = tuple_list[0]
-
             return
+
+    #  print table with n elements in each column, tabulate library used to enhance displaying purposes.
+    def get_max_profit(self):
+        if self.valid_list:
+            self.find_maximum_values()
+            self.print_list()
+            if self.best_choice[2] <= 0:
+                print("There is no best day to buy and sell :(.")
+            else:
+                print("\nThe local left indexes are: " + str(self.left_max))
+                print("The local right indexes are: " + str(self.right_max))
+                print("The local cross indexes are: " + str(self.cross_max) + "\n")
+                print("The best day to buy is day " + str(self.best_choice[0] + 1)
+                      + ", the best day to sell is day " + str(self.best_choice[1])
+                      + ". Total profit: " + str(self.best_choice[2]) + ".")
+
+        else:
+            print("Not enough elements in list.")
 
     def print_list(self):
         if len(self.price_array) <= 0:
@@ -131,11 +132,12 @@ class MaxProfit:
 
         COLUMN_MAX_ELEMENTS = 25  # number of columns per line.
         current_index = 0
-        daily_change_list_copy = [""] + self.daily_change_list.copy()
+        daily_change_list_copy = [""] + self.daily_change_list.copy()  # format with blank space for table
 
-        def print_array_with_column_format(low, max):
-            table = [["Price"] + self.price_array[low:max], ["Change"] + daily_change_list_copy[low:max]]
-            headers = ["Day"] + list(np.arange(low + 1, max + 1))
+        def print_array_with_column_format(low_index, max_index):
+            table = [["Price"] + self.price_array[low_index:max_index], ["Change"]
+                     + daily_change_list_copy[low_index:max_index]]
+            headers = ["Day"] + list(np.arange(low_index + 1, max_index + 1))
 
             print(tabulate(table, headers, tablefmt="simple_grid"))
 
@@ -147,6 +149,38 @@ class MaxProfit:
             current_index += COLUMN_MAX_ELEMENTS
 
 
+# function used to test and compare main program, output might differ in certain edge cases.
+def test_subarray_sum(array):
+    maxint = math.inf
+    transformed_array = []
+    for i in range(len(array) - 1):
+        transformed_array.append(array[i + 1] - array[i])
+
+    max_so_far = -maxint - 1
+    max_ending_here = 0
+
+    for i in range(0, len(transformed_array)):
+        max_ending_here = max_ending_here + transformed_array[i]
+        if max_so_far < max_ending_here:
+            max_so_far = max_ending_here
+
+        if max_ending_here < 0:
+            max_ending_here = 0
+
+    return max_so_far
+
+
+# generate new random list with assigned values
+def random_list_generator(list_size, min_value, max_value):
+    random_generated_list = []
+    for i in range(0, list_size):
+        n = random.randint(min_value, max_value)
+        random_generated_list.append(n)
+
+    #  print(random_generated_list)
+    return random_generated_list
+
+
 def main():
     # sample test cases, edge cases included.
     a = [80, 120, 97, 60, 55, 102, 101, 54, 109, 64, 96, 69, 114, 110, 51, 56, 73, 72, 73, 67, 68, 54, 62, 82, 95, 94,
@@ -156,9 +190,9 @@ def main():
     b = [100, 113, 110, 85, 105, 102, 86, 63, 81, 101, 94, 106, 101, 79, 94, 90, 97]
     c = [7]
     d = []
-    e = random_list_generator(50, 50, 120)
+    e = random_list_generator(100, 50, 120)
     f = [100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 0]
-    test_options = [a, b, c, d, e, f]
+
     tests = []
 
     testcase_a = MaxProfit(a)
@@ -169,34 +203,22 @@ def main():
     testcase_f = MaxProfit(f)
 
     # extra function added to compare algorithm implemented.
-    tests.append({'input': testcase_a.best_choice[2], 'output': max_subarray_sum(a)})
-    tests.append({'input': testcase_b.best_choice[2], 'output': max_subarray_sum(b)})
+    tests.append({'input': testcase_a.best_choice[2], 'output': test_subarray_sum(a)})
+    tests.append({'input': testcase_b.best_choice[2], 'output': test_subarray_sum(b)})
     tests.append({'input': testcase_c.best_choice[2], 'output': 0})  # only one day, so 0.
     tests.append({'input': testcase_d.best_choice[2], 'output': 0})  # max_subarray_sum takes negative values, so 0.
-    tests.append({'input': testcase_e.best_choice[2], 'output': max_subarray_sum(e)})
-    tests.append({'input': testcase_f.best_choice[2], 'output': max_subarray_sum(f)})
+    tests.append({'input': testcase_e.best_choice[2], 'output': test_subarray_sum(e)})
+    tests.append({'input': testcase_f.best_choice[2], 'output': test_subarray_sum(f)})
 
+    print("Test cases using happy and edge cases.\n")
     for i in range(len(tests)):
         print(f'Test {i + 1} Pass:',
               tests[i]['input'] == tests[i]['output'])
     print("///////////////////////////////////////////////////////\n")
-    #
-    # custom_test_case = MaxProfit(f)
-    # if custom_test_case.valid_list:
-    #     custom_test_case.find_maximum_values()
-    #     custom_test_case.print_list()
-    #     if custom_test_case.best_choice[2] <= 0:
-    #         print("There is no best day to buy and sell :(.")
-    #     else:
-    #         print("The local left indexes are: " + str(custom_test_case.left_max))
-    #         print("The local left indexes are: " + str(custom_test_case.right_max))
-    #         print("The local left indexes are: " + str(custom_test_case.cross_max))
-    #         print("The best day to buy is day " + str(custom_test_case.best_choice[0] + 1)
-    #               + ", the best day to sell is day " + str(custom_test_case.best_choice[1])
-    #               + ". Total profit: " + str(custom_test_case.best_choice[2]) + ".")
-    #
-    # else:
-    #     print("Not enough elements in list.")
+
+    # max profit variable can be changed to selected desired test case, choose e for random generated input
+    custom_test_case = MaxProfit(e)
+    custom_test_case.get_max_profit()
 
 
 if __name__ == '__main__':
